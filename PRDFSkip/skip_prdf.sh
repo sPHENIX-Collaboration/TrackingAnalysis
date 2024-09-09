90638afb01a3c201417932d4e2c23a9006d87089
Joe Osborn <osbornjd91@gmail.com>
2024-09-09 15:58:16 -0400

    fix file naming gl1

--- PRDFSkip/skip_prdf.sh
+++ PRDFSkip/skip_prdf.sh
@@ -27,10 +27,10 @@ gl1bco=$(ddump -i $gl1daqname | grep "Beam Clock: " | awk '{print $3}')
 # shift to 40 bit to match tracker bco
 gl1bcotrb=$(($gl1bco & (1 << 40) -1))
 
+ls ${PWD}/$gl1daqname > gl1daq_skip$1-${gl1segment}.list
+
 #make the tpc file list
 sh make_tpc_skipped_prdflist.sh $3 $gl1bcotrb
-
-ls ${PWD}/$gl1daqname > gl1daq_skip$1-${gl1segment}.list
 
 outfile="DST_STREAMING_run2pp_new_2024p002_skip${1}-${runnumber}-${gl1segment}.root"
 
@@ -38,6 +38,6 @@ echo "Processing the job : "
 echo "Nevents "$2
 echo "run number "$3
 echo "outfile "$outfile
-echo "with gl1list "gl1daq_skip$1.list
+echo "with gl1list "gl1daq_skip$1-${gl1segment}.list
 #run the show
-root -l -b -q Fun4All_Stream_Combiner.C\($2,$3,\"$outfile\",\"gl1daq_skip$1.list\",\"tpc00-$gl1bcotrb.list\",\"tpc00-$gl1bcotrb.list\",\"tpc01-$gl1bcotrb.list\",\"tpc02-$gl1bcotrb.list\",\"tpc03-$gl1bcotrb.list\",\"tpc04-$gl1bcotrb.list\",\"tpc05-$gl1bcotrb.list\",\"tpc06-$gl1bcotrb.list\",\"tpc07-$gl1bcotrb.list\",\"tpc08-$gl1bcotrb.list\",\"tpc09-$gl1bcotrb.list\",\"tpc10-$gl1bcotrb.list\",\"tpc11-$gl1bcotrb.list\",\"tpc12-$gl1bcotrb.list\",\"tpc13-$gl1bcotrb.list\",\"tpc14-$gl1bcotrb.list\",\"tpc15-$gl1bcotrb.list\",\"tpc16-$gl1bcotrb.list\",\"tpc17-$gl1bcotrb.list\",\"tpc18-$gl1bcotrb.list\",\"tpc19-$gl1bcotrb.list\",\"tpc20-$gl1bcotrb.list\",\"tpc21-$gl1bcotrb.list\",\"tpc22-$gl1bcotrb.list\",\"tpc23-$gl1bcotrb.list\"\)
+root -l -b -q Fun4All_Stream_Combiner.C\($2,$3,\"$outfile\",\"gl1daq_skip$1-${gl1segment}.list\",\"tpc00-$gl1bcotrb.list\",\"tpc00-$gl1bcotrb.list\",\"tpc01-$gl1bcotrb.list\",\"tpc02-$gl1bcotrb.list\",\"tpc03-$gl1bcotrb.list\",\"tpc04-$gl1bcotrb.list\",\"tpc05-$gl1bcotrb.list\",\"tpc06-$gl1bcotrb.list\",\"tpc07-$gl1bcotrb.list\",\"tpc08-$gl1bcotrb.list\",\"tpc09-$gl1bcotrb.list\",\"tpc10-$gl1bcotrb.list\",\"tpc11-$gl1bcotrb.list\",\"tpc12-$gl1bcotrb.list\",\"tpc13-$gl1bcotrb.list\",\"tpc14-$gl1bcotrb.list\",\"tpc15-$gl1bcotrb.list\",\"tpc16-$gl1bcotrb.list\",\"tpc17-$gl1bcotrb.list\",\"tpc18-$gl1bcotrb.list\",\"tpc19-$gl1bcotrb.list\",\"tpc20-$gl1bcotrb.list\",\"tpc21-$gl1bcotrb.list\",\"tpc22-$gl1bcotrb.list\",\"tpc23-$gl1bcotrb.list\"\)

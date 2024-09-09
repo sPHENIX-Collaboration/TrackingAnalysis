6fb3bb55e4baa287d74a6ee422ad51f0c2511285
Joe Osborn <osbornjd91@gmail.com>
2024-09-09 09:52:30 -0400

    python script to submit many jobs

---
+++ PRDFSkip/submit_skipped_jobs.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+starting_event_numbers = [0,1000000,2000000,3000000,4000000,5000000,6000000,7000000,8000000,9000000,10000000]
+runnumber = 52593
+nEvent = 1000
+ngl1segments = 2
+
+print(starting_event_numbers)
+for gl1segment in range(ngl1segments):
+    gl1daqname = "GL1_physics_gl1daq-{:0>8}-{:0>4}.root".format(runnumber, gl1segment)
+    print("Gl1 name " + gl1daqname)
+    for skip in starting_event_numbers:
+        command = "submit_condor.sh " + str(runnumber) + " " + str(skip) + " " + str(nEvent) + " " + gl1daqname
+        print(command)
+        #subprocess.run()

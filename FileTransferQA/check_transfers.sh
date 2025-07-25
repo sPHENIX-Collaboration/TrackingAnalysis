0a4361f8e71351129e0ecd3b06e7a589ba7e94ec
osbornjd <osbornjd91@gmail.com>
2025-07-25 15:29:38 -0400

    both run at the same time

--- FileTransferQA/check_transfers.sh
+++ FileTransferQA/check_transfers.sh
@@ -7,13 +7,11 @@ logfile="transfer_checking.log"
 yesterday=$(date --date="yesterday" +"%Y-%m-%d")
 
 echo "Date to check " ${yesterday}
-root -b -q CheckFileTransfer.C\(\"${yesterday}\"\) >& ${logfile}
+root -b -q CheckFileTransfer.C\(\"${yesterday}\"\) >& ${logfile} 2>&1
 fullruns=$(grep  'Complete runs (all segments):' ${logfile})
 allseginbraces=$(echo "${fullruns}" | sed -n 's/.*{ *\([^}]*\)*}.*/\1/p')
-root -b -q CheckFileTransfer.C\(\"${yesterday}\",true\) >& ${logfile}
-
-fullruns=$(grep "Complete runs (first segment):" ${logfile})
-zeroseginbraces=$(echo "${fullruns}" | sed -n 's/.*{ *\([^}]*\) *}.*/\1/p')
+fullruns0=$(grep "Complete runs (first segment):" ${logfile})
+zeroseginbraces=$(echo "${fullruns0}" | sed -n 's/.*{ *\([^}]*\) *}.*/\1/p')
 
 echo "Full runs:     "${allseginbraces}"
 0th segment runs "${zeroseginbraces}"

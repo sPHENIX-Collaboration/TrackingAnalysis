b4f554a50a3638304660552cb4f186fda867b856
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2025-07-25 17:40:32 -0400

    - Some cleanup - remove first_segment_only - Generate on TCanvas per run, with File Transfer summary QA, suitable for Offline QA pages.

--- FileTransferQA/CheckFileTransfer.C
+++ FileTransferQA/CheckFileTransfer.C
@@ -40,7 +40,7 @@ namespace
   }
 
   // hardcoded list of all tracking subsystems
-  static const subsystem_info_t::list subsystems = {
+  static const subsystem_info_t::list default_subsystems = {
     // TPC subsystems
     {"tpc", "TPC", "ebdc00" },
     {"tpc", "TPC", "ebdc01" },
@@ -173,10 +173,6 @@ namespace
 
   // verbosity
   static constexpr int verbosity = 0;
-
-  // files missing from database
-  filename_set_t files_missing_from_db;
-
 }
 
 //_________________________________________________________
@@ -453,104 +449,35 @@ runnumber_set_t get_runnumbers( const filename_set_t& in )
 }
 
 //_________________________________________________________
-void CheckFileTransfer_from_bbox(
-  const runnumber_set_t runnumbers = {},
-  const subsystem_info_t& subsystem = default_subsystem,
-  const std::string& runtype = default_runtype
-)
+TH1* create_subsytems_histogram( const std::string& name, const std::string& title, const subsystem_info_t::list& subsystems = default_subsystems )
 {
 
-  std::cout << "CheckFileTransfer_from_bbox - subsystem: " << subsystem << std::endl;
+  const size_t count = subsystems.size();
+  auto h = new TH1F( name.c_str(), title.c_str(), count, 0, count );
 
-  // list of files on buffer boxes and check if transferd
-  const auto bbox_files = get_files_from_bbox( runnumbers, subsystem, runtype );
+  // assign x axis bin labels
+  for( size_t i = 0; i < count; ++i )
+  { h->GetXaxis()->SetBinLabel( i+1, (subsystems[i].subsystem+"/"+subsystems[i].host).c_str()); }
 
-  if( true )
-  { std::cout << "CheckFileTransfer_from_bbox - files in bufferbox: " << bbox_files << std::endl; }
+  h->GetYaxis()->SetTitle( "segment count" );
 
-  // get corresponding set of runnumbers
-  const auto bbox_runnumbers = get_runnumbers(bbox_files);
-
-  // get files from daq database
-  const auto daqdb_files = get_files_from_db(bbox_runnumbers, subsystem, runtype);
-
-  if( true )
-  { std::cout << "CheckFileTransfer_from_bbox - files in db: " << daqdb_files << std::endl; }
-
-  if( true )
-  {
-    // compare bbox and database
-    filename_set_t found_files;
-    filename_set_t missing_files;
-
-    // check transfer status
-    for( const auto& source:bbox_files )
-    {
-      const bool found = std::find_if(daqdb_files.begin(), daqdb_files.end(),
-        [&source](const fileinfo_t& fileinfo ) { return fileinfo.filename == source; })
-        != daqdb_files.end();
-
-      if( found ) found_files.insert(source);
-      else missing_files.insert(source);
-    }
-
-    std::cout << "CheckFileTransfer_from_bbox - missing from db: " << missing_files << std::endl;
-    files_missing_from_db.merge(missing_files);
-  }
-
-  if( true )
-  {
-    // check whether files have been transfered or not
-    // get corresponding lustre files
-    const auto lustre_files = get_files_from_lustre(bbox_runnumbers);
-    const auto lustre_local_files = get_local_filenames(lustre_files);
-
-    filename_set_t transfered_files;
-    filename_set_t missing_files;
-    filename_set_t inconsistent_files;
-
-    // check transfer status
-    for( const auto& source:bbox_files )
-    {
-      const bool transfered = (lustre_local_files.find(get_local_filename(source))!=lustre_local_files.end());
-      if( transfered ) transfered_files.insert(source);
-      else
-      {
-        missing_files.insert(source);
-
-        // find matching daq_db file info
-        const auto iter = std::find_if(daqdb_files.begin(), daqdb_files.end(),
-          [&source](const fileinfo_t& fileinfo ) { return fileinfo.filename == source; });
-        if( iter != daqdb_files.end() && iter->in_sdcc )
-        { inconsistent_files.insert(source); }
-      }
-    }
-
-    std::cout << "CheckFileTransfer_from_bbox - missing from sdcc: " << missing_files << std::endl;
-    std::cout << "CheckFileTransfer_from_bbox - missing and inconsistent with db: " << inconsistent_files << std::endl;
-
-  }
+  return h;
 }
 //
 // start_date
 // date should look like
 // XXXX-XX-XX
 // as in 2025-07-21
-// first_segment_only=true - check only if the 0th segment is transferred
 //
 //_________________________________________________________
-void CheckFileTransfer(const std::string& date,
-		       const bool first_segment_only = false)
+void CheckFileTransfer(const std::string& start_date )
 {
-
   // get list of runs
-  static const std::string start_date = date;
   const auto runnumbers = get_runnumbers_from_db( start_date );
 
   // map runnumber with missing subsystems
   std::map<int, subsystem_info_t::list> missing;
   std::map<int, subsystem_info_t::list> missing_first_segment;
-
 
   // set to true to verify if transfered files are effectively on disk
   const bool check_db_consistency = false;
@@ -563,11 +490,26 @@ void CheckFileTransfer(const std::string& date,
     // if( verbosity )
     { std::cout << "runnumber: " << runnumber << std::endl; }
 
+    // create histograms
+    std::unique_ptr<TH1> h_ref(create_subsytems_histogram( "h_ref", "total number of segments", default_subsystems ) );
+    std::unique_ptr<TH1> h_transfered(create_subsytems_histogram( "h_transfered", "transfered segments", default_subsystems ) );
+    std::unique_ptr<TH1> h_transfered_first_segment(create_subsytems_histogram( "h_first_segment_transfered", "first segment transfered", default_subsystems ) );
+
+    // counters
+    unsigned int n_segments_total = 0;
+    unsigned int n_first_segment_total = 0;
+
+    unsigned int n_segments_transfered = 0;
+    unsigned int n_first_segment_transfered = 0;
+
     // loop over subsystems
-    for( const auto& subsystem:subsystems )
+    for( size_t i = 0; i<default_subsystems.size(); ++i )
     {
+      // get subsystem
+      const auto& subsystem = default_subsystems[i];
+
       // get the files from database
-      const auto& daqdb_files = get_files_from_db( {runnumber}, subsystem );
+      const auto daqdb_files = get_files_from_db( {runnumber}, subsystem );
 
       if( verbosity )
       { std::cout << "subsystem: " << subsystem << " files: " << daqdb_files << std::endl; }
@@ -577,8 +519,13 @@ void CheckFileTransfer(const std::string& date,
       bool transferred_first_segment = true;
       for( const auto& file_info:daqdb_files )
       {
+        h_ref->Fill(i+1);
+
         const bool is_first_segment = (get_run_segment(file_info.filename).second == 0);
-        if( first_segment_only && !is_first_segment ) { continue; }
+
+        // increment counters
+        ++n_segments_total;
+        if( is_first_segment ) { ++n_first_segment_total; }
 
         if( check_db_consistency && file_info.in_sdcc )
         {
@@ -594,14 +541,94 @@ void CheckFileTransfer(const std::string& date,
           if( is_first_segment )
           {
             transferred_first_segment = false;
-            if( !check_db_consistency ) { break; }
           }
+        } else {
+
+          // fill histograms
+          h_transfered->Fill(i+1);
+          if( is_first_segment )
+          { h_transfered_first_segment->Fill(i+1); }
+
+          // increment counters
+          ++n_segments_transfered;
+          if( is_first_segment ) { ++n_first_segment_transfered; }
+
         }
+
       }
 
       if(!transferred) { missing[runnumber].emplace_back(subsystem); }
       if(!transferred_first_segment) { missing_first_segment[runnumber].emplace_back(subsystem); }
+
     }
+
+    // make canvas and save
+    std::unique_ptr<TCanvas> cv( new TCanvas( "cv", "cv", 1200, 1200 ) );
+
+    cv->Divide(1,2 );
+
+    // adjust pad dimensions
+    cv->GetPad(1)->SetPad(0, 0.3, 1, 1);
+    cv->GetPad(2)->SetPad(0, 0, 1, 0.3);
+
+    // status histogram
+    cv->cd(1);
+    h_ref->SetStats(0);
+    h_ref->SetTitle(Form( "File transfer status for run %i", runnumber ));
+    h_ref->SetFillStyle(1001);
+    h_ref->SetFillColor(kYellow-10);
+    h_ref->SetMinimum(0.5);
+    h_ref->GetXaxis()->SetLabelSize(0.03);
+    h_ref->Draw("h");
+
+    h_transfered->SetFillStyle(1001);
+    h_transfered->SetFillColor(kGreen-8);
+    h_transfered->Draw("h same");
+
+    h_transfered_first_segment->SetFillStyle(1001);
+    h_transfered_first_segment->SetFillColor(kGreen-5);
+    h_transfered_first_segment->Draw("h same");
+
+    // legend
+    auto legend = new TLegend( 0.7, 0.7, 0.95, 0.85, "", "NDC" );
+    legend->SetFillStyle(0);
+    legend->AddEntry( h_ref.get(), "all segments", "f" );
+    legend->AddEntry( h_transfered.get(), "transfered", "f" );
+    legend->AddEntry( h_transfered_first_segment.get(), "first segment transfered", "f" );
+    legend->Draw();
+
+    // gPad->SetTopMargin(0.01);
+    gPad->SetLogy();
+
+    // summary
+    cv->cd(2);
+    auto text = new TPaveText(0.1,0.1,0.9,0.9, "NDC" );
+    text->SetFillColor(0);
+    text->SetFillStyle(0);
+    text->SetBorderSize(0);
+    text->SetTextAlign(11);
+
+    text->AddText( "File transfer summary:" );
+
+    if( n_segments_transfered == n_segments_total )
+    {
+      text->AddText( Form("Number of files transfered: %i/%i - good",n_segments_transfered,n_segments_total ))->SetTextColor(kGreen+1);
+    } else {
+      text->AddText( Form("Number of files transfered: %i/%i - bad",n_segments_transfered,n_segments_total ))->SetTextColor(kRed+1);
+    }
+
+    if( n_first_segment_transfered == n_first_segment_total )
+    {
+      text->AddText( Form("Number of first segment files transfered: %i/%i - good",n_first_segment_transfered,n_first_segment_total ))->SetTextColor(kGreen+1);
+    } else {
+      text->AddText( Form("Number of first segment files transfered: %i/%i - bad",n_first_segment_transfered,n_first_segment_total ))->SetTextColor(kRed+1);
+    }
+
+    text->Draw();
+
+    // save canvas
+    cv->SaveAs( Form("FileTransferQA_0_%i.png", runnumber));
+
   }
 
   // print summary
@@ -610,19 +637,16 @@ void CheckFileTransfer(const std::string& date,
   std::cout << "runnumbers: " << runnumbers << std::endl << std::endl;
 
   // get list of fully transfered runs
-  if( !first_segment_only )
-  {
-    runnumber_set_t complete_runs;
-    std::copy_if( runnumbers.begin(), runnumbers.end(), std::inserter(complete_runs, complete_runs.end()),
-      [&missing]( const int& runnumber ) { return !missing.contains(runnumber); } );
-    std::cout << "Complete runs (all segments): " << complete_runs << std::endl << std::endl;
+  runnumber_set_t complete_runs;
+  std::copy_if( runnumbers.begin(), runnumbers.end(), std::inserter(complete_runs, complete_runs.end()),
+    [&missing]( const int& runnumber ) { return !missing.contains(runnumber); } );
+  std::cout << "complete runs (all segments): " << complete_runs << std::endl << std::endl;
 
-    // get list of incomplete runs
-    runnumber_set_t incomplete_runs;
-    std::copy_if( runnumbers.begin(), runnumbers.end(), std::inserter(incomplete_runs, incomplete_runs.end()),
-      [&missing]( const int& runnumber ) { return missing.contains(runnumber); } );
-    std::cout << "incomplete runs (all segments): " << incomplete_runs << std::endl << std::endl;
-  }
+  // get list of incomplete runs
+  runnumber_set_t incomplete_runs;
+  std::copy_if( runnumbers.begin(), runnumbers.end(), std::inserter(incomplete_runs, incomplete_runs.end()),
+    [&missing]( const int& runnumber ) { return missing.contains(runnumber); } );
+  std::cout << "incomplete runs (all segments): " << incomplete_runs << std::endl << std::endl;
 
   // get list of fully transfered runs (first segment)
   runnumber_set_t complete_runs_first_segment;
